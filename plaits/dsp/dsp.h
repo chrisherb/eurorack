4a6be6f8539e95cd06d0a942c210dfe231939be6
Chris <chrisherb.sites@outlook.com>
2023-07-23 20:43:04 +0200

    raised maximum block size

--- plaits/dsp/dsp.h
+++ plaits/dsp/dsp.h
@@ -32,7 +32,7 @@
 #include "stmlib/stmlib.h"
 
 namespace plaits {
-  
+
 static const float kSampleRate = 48000.0f;
 
 // There is no proper PLL for I2S, only a divider on the system clock to derive
@@ -47,7 +47,7 @@ static const float kSampleRate = 48000.0f;
 static const float kCorrectedSampleRate = 47872.34f;
 const float a0 = (440.0f / 8.0f) / kCorrectedSampleRate;
 
-const size_t kMaxBlockSize = 24;
+const size_t kMaxBlockSize = 64;
 const size_t kBlockSize = 12;
 
 }  // namespace plaits
